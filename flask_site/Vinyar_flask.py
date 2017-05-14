0171bf7197b8e4507251acc4677dce39a033e919
alexvinyar <alexvinyar@yandex.ru>
2017-05-15 01:56:21 +0400

    updated site - added charts

--- flask_site/Vinyar_flask.py
+++ flask_site/Vinyar_flask.py
@@ -20,9 +20,10 @@ def count_members(id_1, id_2):
     both_members = len(members_1.intersection(members_2))
     members_1_num = first_group['response'][0]['members_count']
     members_2_num = second_group['response'][0]['members_count']
+    d = {'Group 1' : members_1_num, 'Group 2' : members_2_num}
     return 'Количество участников в первой группе: ' + str(members_1_num) + '<br>' + \
            'Количество участников во второй группе: ' + str(members_2_num) + '<br>' + \
-           'Пересекающиеся участники: ' + str(both_members)
+           'Пересекающиеся участники: ' + str(both_members), d
 
 @app.route('/')
 def start():
@@ -30,6 +31,7 @@ def start():
 
 @app.route('/verb_page', methods=['get', 'post'])
 def verb_page():
+    d = {}
     if request.form:
         text = request.form['text']
         analysis = m.analyze(text)
@@ -44,35 +46,41 @@ def verb_page():
             if i['text'].strip() and 'analysis' in i and i['analysis']:
                 all_wrds += 1
                 gr_info = i['analysis'][0]['gr']
-                if 'V' in gr_info:
+                if 'V,' in gr_info:
                     all_verbs += 1
                     verb_arr.append(i['analysis'][0]['lex'])
-                if 'несов' not in gr_info:
+                if 'V,' in gr_info and 'несов' not in gr_info:
                     perfect += 1
-                if 'несов' in gr_info:
+                if 'V,' and 'несов' in gr_info:
                     imperfect += 1
-                if 'пе=' in gr_info:
+                if 'V,' and 'пе=' in gr_info:
                     transitive += 1
-                if 'нп=' in gr_info:
+                if 'V,' and 'нп=' in gr_info:
                     intransitive += 1
         result = 'verbs: ' + str(all_verbs) + '<br>' + \
                  '% of verbs: ' + str((all_verbs/all_wrds)*100) + '<br>' + \
                  'perfect: ' + str(perfect) + '<br>' + \
                  'imperfect: ' + str(imperfect) + '<br>' + \
                  'transitive: ' + str(transitive) + '<br>' + \
-                 'intransitive: ' + str(intransitive)
-        return render_template('verb_page.html', input=text, text=result)
-    return render_template('verb_page.html')
+                 'intransitive: ' + str(intransitive) + '<br>' + \
+                 'verb frequency: ' + str(Counter(verb_arr))
+        dic = Counter(verb_arr).items()
+        for i in dic:
+            d.update(d.fromkeys(i[:1], i[1]))
+        return render_template('verb_page.html', input=text, text=result, data=d)
+    return render_template('verb_page.html', data={})
 
 @app.route('/vk_page', methods=['get', 'post'])
 def vk_page():
+    d = {}
     if request.form:
         text = request.form['text'].split('\n')
         id_1 = text[0]
         id_2 = text[1]
-        result = count_members(id_1, id_2)
-        return render_template('vk_page.html', input=text, text=result)
-    return render_template('vk_page.html')
+        result = count_members(id_1, id_2)[0]
+        d.update(count_members(id_1, id_2)[1])
+        return render_template('vk_page.html', input=text, text=result, data=d)
+    return render_template('vk_page.html', data={})
 
 if __name__ == '__main__':
     app.run(debug=True)
